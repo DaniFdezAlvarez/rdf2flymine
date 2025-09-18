56ba1e7f6bc2acb76eedbae5e28801b4c9e8b457
DaniFdezAlvarez <danifdezalvarez@gmail.com>
2025-09-18 20:02:42 +0200

    Working on shex2flymine. Shex parsed, adding links to classes

--- shex2flymine.py
+++ shex2flymine.py
@@ -1,4 +1,5 @@
 from pyshexc.parser_impl.generate_shexj import parse as parse_shex
+from ShExJSG.ShExJ import Shape
 import os.path as pth
 
 _PTH_SEP = pth.sep
@@ -28,6 +29,9 @@ _STATUS_CODE = "statusCode"
 
 _TODO = "WARNING! TO-DO"
 
+#  URIS
+
+_RDF_tYPE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
 
 class Utils(object):
 
@@ -35,6 +39,15 @@ class Utils(object):
     def load_file(file_path: str):
         with open(file_path, encoding="utf-8") as in_stream:
             return in_stream.read()
+
+    @staticmethod
+    def uri_to_var_camel_case(uri: str):
+        pass  # TODO
+
+
+    @staticmethod
+    def uri_to_readable_label(uri: str):
+        pass  # TODO
 
 
 class Shex2Flymine(object):
@@ -60,9 +73,54 @@ class Shex2Flymine(object):
 
     def _map_shapes(self):
         for a_shape in self._shex_model.shapes:
-            print("-----")
-            for exp in a_shape.expression.expressions:
-                print (exp)
+            print(type(a_shape))
+            shape_var = Utils.uri_to_var_camel_case(a_shape.id)
+            shape_label = Utils.uri_to_readable_label(a_shape.id)
+            target_class_node = Shex2Flymine._empty_class_node()
+            self._json_model[shape_var] = target_class_node
+            target_class_node[_DISPLAY_NAME] = shape_label
+            target_class_node[_NAME] = shape_var
+            target_class_node[_TERM] = self._look_for_shape_class(a_shape)
+            self._fill_class_links(a_shape, target_class_node)
+
+            # print("-----")
+            # for exp in a_shape.expression.expressions:
+            #     print (exp)
+
+    def _fill_class_links(self, shape: Shape, target_class_node: dict):
+        for exp in shape.expression.expressions:
+            if self._is_typing_exp(exp):
+                pass  # Skip it, do nothing
+            elif self._has_literal_object(exp):
+                self._add_att_entry(shape, exp)
+            elif self._has_unbounded_cardinality(exp):
+                self._add_collections_entry(shape,exp)
+            else:  # The object is a shape and the max cardinality must be 1
+                self._add_reference_entry(shape, exp)
+
+    def _add_reference_entry(self, shape, exp):
+        pass
+
+    def _add_collections_entry(self, shape, exp):
+        pass
+
+    def _has_unbounded_cardinality(self, exp):
+        pass
+
+    def _add_att_entry(self, shape, exp):
+        pass
+
+    def _has_literal_object(self, exp):
+        pass
+
+    def _is_typing_exp(self, exp):
+        pass
+
+    #     print (exp)
+    def _look_for_shape_class(self, a_shape: Shape):
+        for exp in a_shape.expression.expressions:
+            if str(exp.predicate) == _RDF_tYPE:
+                return str(exp.valueExpr.values[0])
 
     @staticmethod
     def _empty_skeleton_model():
